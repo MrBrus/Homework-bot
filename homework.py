6ce7420eb3bacf23e37fa41b5d50ac63936cdd32
Vlad Brusensky <brusen_sky@mail.ru>
2022-04-26 00:56:45 +0300

    Спринт 7 Первый пуш

--- homework.py
+++ homework.py
@@ -11,9 +11,9 @@ from telegram.ext import Updater
 
 load_dotenv()
 
-PRACTICUM_TOKEN = os.getenv('PRACTICUM_TOKEN')
-TELEGRAM_TOKEN = os.getenv('TELEGRAM_TOKEN')
-TELEGRAM_CHAT_ID = os.getenv('TELEGRAM_CHAT_ID')
+PRACTICUM_TOKEN = os.getenv("PRACTICUM_TOKEN")
+TELEGRAM_TOKEN = os.getenv("TELEGRAM_TOKEN")
+TELEGRAM_CHAT_ID = os.getenv("TELEGRAM_CHAT_ID")
 
 RETRY_TIME = 600
 ENDPOINT = 'https://practicum.yandex.ru/api/user_api/homework_statuses/'
